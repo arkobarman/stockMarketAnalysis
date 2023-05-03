c87cc61fefc524910c165a3fa574915c12654b41
arkobarman <arkoju@gmail.com>
2023-05-03 15:24:40 -0500

    fixed negative yerr Error in stochastic oscillators

--- APIs/stochasticOscillators.py
+++ APIs/stochasticOscillators.py
@@ -219,8 +219,8 @@ def computeAndPlotSlowStochasticOscillator(companyName='GOOG', n1=14, n2=3, star
     lowList = dataFr['Low'].to_list()
     dateList = dataFr['Date'].to_list()
     closeList = dataFr['Close'].to_list()
-    lowerError = [a-b for a, b in zip(closeList, lowList)]
-    upperError = [a-b for a, b in zip(highList, closeList)]
+    lowerError = [a-min(a,b) for a, b in zip(closeList, lowList)]
+    upperError = [max(a,b)-b for a, b in zip(highList, closeList)]
     ax0.errorbar(x=dateList, y=closeList, yerr=[lowerError, upperError], fmt='o')
     for n, label in enumerate(ax0.xaxis.get_ticklabels()):
             if n % every_nth != 0:
@@ -278,8 +278,8 @@ def computeAndPlotFullStochasticOscillator(companyName='GOOG', n1=14, n2=3, n3=3
     lowList = dataFr['Low'].to_list()
     dateList = dataFr['Date'].to_list()
     closeList = dataFr['Close'].to_list()
-    lowerError = [a-b for a, b in zip(closeList, lowList)]
-    upperError = [a-b for a, b in zip(highList, closeList)]
+    lowerError = [a-min(a,b) for a, b in zip(closeList, lowList)]
+    upperError = [max(a,b)-b for a, b in zip(highList, closeList)]
     ax0.errorbar(x=dateList, y=closeList, yerr=[lowerError, upperError], fmt='o')
     for n, label in enumerate(ax0.xaxis.get_ticklabels()):
             if n % every_nth != 0:
